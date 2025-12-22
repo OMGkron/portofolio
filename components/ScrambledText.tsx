7c13b4f112ca644a649c2ec26b41900febfc0132
Mac <mac@Macs-MacBook-Air.local>
2025-12-22 15:06:07 +0700

    fix typescript props for ScrambledText

--- components/ScrambledText.tsx
+++ components/ScrambledText.tsx
@@ -6,20 +6,39 @@ import ScrambleTextPlugin from "gsap/ScrambleTextPlugin";
 
 gsap.registerPlugin(ScrambleTextPlugin);
 
-export default function ScrambledText({ text, className = "" }) {
-  const ref = useRef(null);
+type ScrambledTextProps = {
+  text: string;
+  className?: string;
+};
+
+export default function ScrambledText({
+  text,
+  className = "",
+}: ScrambledTextProps) {
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const runScramble = () => {
+    if (!ref.current) return;
+
     gsap.fromTo(
       ref.current,
-      { scrambleText: { text: text, chars: "0123456789", revealDelay: 0 } },
-      { duration: 5, scrambleText: text }
+      {
+        scrambleText: {
+          text,
+          chars: "0123456789",
+          revealDelay: 0,
+        },
+      },
+      {
+        duration: 5,
+        scrambleText: text,
+      }
     );
   };
 
   useEffect(() => {
     runScramble();
-  }, []);
+  }, [text]);
 
   return (
     <div
